d3b9db3dec9544a479f4bf3b0f7212bba50d960a
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-04-14 13:19:42 -0500

    docs: add missing comments

--- executor/linux/step.go
+++ executor/linux/step.go
@@ -293,6 +293,8 @@ func (c *client) DestroyStep(ctx context.Context, ctn *pipeline.Container) error
 	defer func() {
 		c.logger.Info("uploading step snapshot")
 		// send API call to update the step
+		//
+		// https://pkg.go.dev/github.com/go-vela/sdk-go/vela?tab=doc#StepService.Update
 		_, _, err := c.Vela.Step.Update(c.repo.GetOrg(), c.repo.GetName(), c.build.GetNumber(), step)
 		if err != nil {
 			c.logger.Errorf("unable to upload step snapshot: %v", err)

af34f6ed5d71ab612bbec80f04af571c92a0ddc2
Jordan <Jordan.Brockopp@target.com>
2021-01-11 12:46:07 -0600

    chore: add nolint comment for gomnd v2

--- executor/local/step.go
+++ executor/local/step.go
@@ -230,6 +230,10 @@ func (c *client) DestroyStep(ctx context.Context, ctn *pipeline.Container) error
 	// check if the step is in a pending state
 	if s.GetStatus() == constants.StatusPending {
 		// update the step fields
+		//
+		// TODO: consider making this a constant
+		//
+		// nolint: gomnd // ignore magic number 137
 		s.SetExitCode(137)
 		s.SetFinished(time.Now().UTC().Unix())
 		s.SetStatus(constants.StatusKilled)

470a3c76fcbed4d60a7412f44aae50ee147251b1
Matthew.Fevold <matthew.fevold@target.com>
2021-02-04 13:19:59 -0600

    feat(api): update cancel builds to cancel non successful steps,stages,services

--- executor/local/api.go
+++ executor/local/api.go
@@ -7,8 +7,11 @@ package local
 import (
 	"fmt"
 	"os"
+	"strings"
 	"syscall"
 
+	"github.com/go-vela/pkg-executor/internal/service"
+	"github.com/go-vela/pkg-executor/internal/step"
 	"github.com/go-vela/types/constants"
 	"github.com/go-vela/types/library"
 	"github.com/go-vela/types/pipeline"
@@ -54,6 +57,72 @@ func (c *client) CancelBuild() (*library.Build, error) {
 
 	// set the build status to canceled
 	b.SetStatus(constants.StatusCanceled)
+
+	// get the current build from the client
+	pipeline, err := c.GetPipeline()
+	if err != nil {
+		return nil, err
+	}
+
+	// cancel non successful services
+	for _, _service := range pipeline.Services {
+		// load the service from the client
+		//
+		// https://pkg.go.dev/github.com/go-vela/pkg-executor/internal/service#Load
+		s, err := service.Load(_service, &c.services)
+		if err != nil {
+			// create the service from the container
+			//
+			// https://pkg.go.dev/github.com/go-vela/types/library#ServiceFromContainer
+			s = library.ServiceFromContainer(_service)
+		}
+
+		// if service has not succeeded, set it as canceled
+		if !strings.EqualFold(s.GetStatus(), constants.StatusSuccess) {
+			s.SetStatus(constants.StatusCanceled)
+		}
+	}
+
+	// cancel non successful steps
+	for _, _step := range pipeline.Steps {
+		// load the step from the client
+		//
+		// https://pkg.go.dev/github.com/go-vela/pkg-executor/internal/step#Load
+		s, err := step.Load(_step, &c.steps)
+		if err != nil {
+			// create the step from the container
+			//
+			// https://pkg.go.dev/github.com/go-vela/types/library#StepFromContainer
+			s = library.StepFromContainer(_step)
+		}
+
+		// if step has not succeeded, set it as canceled
+		if !strings.EqualFold(s.GetStatus(), constants.StatusSuccess) {
+			s.SetStatus(constants.StatusCanceled)
+		}
+	}
+
+	// cancel non successful stages
+	for _, _stage := range pipeline.Stages {
+		// cancel non successful steps for that stage
+		for _, _step := range _stage.Steps {
+			// load the step from the client
+			//
+			// https://pkg.go.dev/github.com/go-vela/pkg-executor/internal/step#Load
+			s, err := step.Load(_step, &c.steps)
+			if err != nil {
+				// create the step from the container
+				//
+				// https://pkg.go.dev/github.com/go-vela/types/library#StepFromContainer
+				s = library.StepFromContainer(_step)
+			}
+
+			// if step has not succeeded, set it as canceled
+			if !strings.EqualFold(s.GetStatus(), constants.StatusSuccess) {
+				s.SetStatus(constants.StatusCanceled)
+			}
+		}
+	}
 
 	p, err := os.FindProcess(os.Getpid())
 	if err != nil {
